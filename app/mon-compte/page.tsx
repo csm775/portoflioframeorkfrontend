881de7b5f912d47ea16fcbbaac43c2e3f8b83d99
csm775 <cellousowmouvagha@gmail.com>
2026-03-01 23:05:52 +0100

    e

--- app/mon-compte/page.tsx
+++ app/mon-compte/page.tsx
@@ -2,13 +2,20 @@
 
 import { useEffect } from "react";
 import { getSession } from "@/utils/sessions";
- 
-  const logSession = async () => {
-    const session = await getSession();
-    console.log(session);
-  };
- 
+
+export default function MonComptePage() {
   useEffect(() => {
+    const logSession = async () => {
+      const session = await getSession();
+      console.log(session);
+    };
     logSession();
   }, []);
- 
+
+  return (
+    <div>
+      <h1>Mon Compte</h1>
+      <p>Bienvenue sur votre page de compte.</p>
+    </div>
+  );
+}
