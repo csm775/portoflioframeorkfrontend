1e053a76e2c2ee29959ce7fd05c2af528ceb007a
csm775 <cellousowmouvagha@gmail.com>
2026-03-01 22:58:50 +0100

    e

--- app/mon-compte/page.tsx
+++ app/mon-compte/page.tsx
@@ -1,5 +1,7 @@
-import { getSession } from "@/utils/sessions";
+"use client";
+
 import { useEffect } from "react";
+import { getSession } from "@/utils/sessions";
  
   const logSession = async () => {
     const session = await getSession();
